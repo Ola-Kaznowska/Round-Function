158308a0c4f1868845c6a26d2db52d821933716d
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2025-04-28 17:31:57 +0200

    Add files via upload

---
+++ Round Function.py
@@ -0,0 +1,12 @@
+x = float(1.9)
+y = float(0.8)
+
+print(round(6 * x + 2 * y, 6) == 13)
+print()
+print(round(4 * x - 7 * y, 6) == 2)
+print()
+x = 2
+y = -1
+print(round(5 * x + 4 * y, 6) == 6)
+print()
+print(round(2 * x + y, 6) == 3)
